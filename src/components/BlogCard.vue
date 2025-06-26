616251fcdb7d7f24b13d5e656ddd1a0730665c6b
Nikolas Clark <nac09@msn.com>
2025-06-26 16:00:08 -0700

    Drawing blogs via Blog Prop and Card Comp

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { Blogs } from '@/models/Blogs.js';
+
+
+defineProps({
+  blogProp: { type: Blogs, required: true }
+})
+
+</script>
+
+
+<template>
+  <div class="blog-card">
+    <img class="blog-img" :src="blogProp.creator.picture" :alt="`A picture of ${blogProp.creator.name}`">
+    <img height="200" :src="blogProp.imgUrl">
+    <label class="blog-title">{{ blogProp.title }}</label>
+    <p>{{ blogProp.body }}</p>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.blog-card {
+  margin-bottom: 1rem;
+  border-radius: 0px;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    height: auto;
+  }
+
+}
+
+
+.profile-img {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  height: 50px;
+  width: 50px;
+  object-fit: cover;
+  border-radius: 50%;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { AppState } from '@/AppState.js';
+import BlogCard from '@/components/BlogCard.vue';
 import { blogService } from '@/services/BlogService.js';
 import { logger } from '@/utils/Logger.js';
 import { Pop } from '@/utils/Pop.js';
 import { onMounted, computed } from 'vue';
+
 
 
 const blogs = computed(() => AppState.blogs)
@@ -25,7 +27,12 @@ async function getBlogs() {
 </script>
 
 <template>
-  {{ blogs }}
+  <div class="mx-2 mx-md-5">
+    <section v-if="blogs.length" class="blog-cards p-3">
+      <BlogCard v-for="blogProp in blogs" :key="`blog-card-${blogProp.id}`" :blogProp />
+
+    </section>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
